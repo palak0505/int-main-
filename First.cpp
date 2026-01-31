56e77863a1002b8602a253c7b4a50061a1f40ae6
Palak <2241011@sliet.ac.in>
2026-01-31 18:57:16 +0530

    Create First.cpp

---
+++ First.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "🚀 Hello, Code World!" << endl;
+    cout << "👩‍💻 I am Palak, a Tech Curious learner." << endl;
+    return 0;
+}
